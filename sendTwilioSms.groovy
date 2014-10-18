27c1c0e3dfe5195d8f61fe800df8dfab20e56bec
Todd Crone <todd.crone@perceptivesoftware.com>
2014-10-18 19:31:43 -0400

    Add test credentials and magic numbers.

--- sendTwilioSms.groovy
+++ sendTwilioSms.groovy
@@ -6,16 +6,16 @@ import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
 // Find your Account Sid and Token at twilio.com/user/account
-ACCOUNT_SID = args[0]
-AUTH_TOKEN = args[1]
+ACCOUNT_SID = 'ACbee9cae6cca2b82845e057571236378e'
+AUTH_TOKEN = '811da513032f1f8866e9bcb127b167cd'
 
 client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
 
 // Build a filter for the MessageList
 params = new ArrayList<NameValuePair>();
 params.add(new BasicNameValuePair("Body", "Hey!"));
-params.add(new BasicNameValuePair("To", "+18594567890"));
-params.add(new BasicNameValuePair("From", "+18594567890"));
+params.add(new BasicNameValuePair("To", "+15005550006"));
+params.add(new BasicNameValuePair("From", "+15005550006"));
 
 messageFactory = client.getAccount().getMessageFactory();
 message = messageFactory.create(params);
